da727b0bdffa2b0350bbfcfa962df1a59ee5a184
Phạm Ngọc Đông <dong.orc12@gmail.com>
2022-12-31 10:26:58 +0700

    tích hợp autrhentication với openiddict

--- aspnet-core/src/common/domain/PetProjectEcommerce.Domain/OpenIddict/OpenIddictDataSeedContributor.cs
+++ aspnet-core/src/common/domain/PetProjectEcommerce.Domain/OpenIddict/OpenIddictDataSeedContributor.cs
@@ -84,9 +84,16 @@ public class OpenIddictDataSeedContributor : IDataSeedContributor, ITransientDep
             OpenIddictConstants.Permissions.Scopes.Email,
             OpenIddictConstants.Permissions.Scopes.Phone,
             OpenIddictConstants.Permissions.Scopes.Profile,
-            OpenIddictConstants.Permissions.Scopes.Roles,
-            "PetProjectEcommerce"
+            OpenIddictConstants.Permissions.Scopes.Roles
         };
+
+        var adminScopes = new List<string>();
+        adminScopes.AddRange(commonScopes);
+        adminScopes.Add("PetProjectEcommerce.Admin");
+
+        var clientScopes = new List<string>();
+        clientScopes.AddRange(commonScopes);
+        clientScopes.Add("PetProjectEcommerce");
 
         var configurationSection = _configuration.GetSection("OpenIddict:Applications");
 
@@ -94,7 +101,7 @@ public class OpenIddictDataSeedContributor : IDataSeedContributor, ITransientDep
         var adminClientId = configurationSection["PetProjectEcommerce_Admin:ClientId"];
         if (!adminClientId.IsNullOrWhiteSpace())
         {
-            var adminClientRootUrl = configurationSection["PetProjectEcommerce_Admin:RootUrl"].EnsureEndsWith('/');
+            var adminClientRootUrl = configurationSection["PetProjectEcommerce_Admin:RootUrl"].TrimEnd('/');
 
             /* PetProjectEcommerce_Admin client is only needed if you created a tiered
              * solution. Otherwise, you can delete this client. */
@@ -110,10 +117,10 @@ public class OpenIddictDataSeedContributor : IDataSeedContributor, ITransientDep
                     OpenIddictConstants.GrantTypes.RefreshToken,
                     OpenIddictConstants.GrantTypes.Implicit
                 },
-                scopes: commonScopes,
-                redirectUri: $"{adminClientRootUrl}signin-oidc",
+                scopes: adminScopes,
+                redirectUri: adminClientRootUrl,
                 clientUri: adminClientRootUrl,
-                postLogoutRedirectUri: $"{adminClientRootUrl}signout-callback-oidc"
+                postLogoutRedirectUri: adminClientRootUrl
             );
         }
 
@@ -136,10 +143,34 @@ public class OpenIddictDataSeedContributor : IDataSeedContributor, ITransientDep
                     OpenIddictConstants.GrantTypes.AuthorizationCode,
                     OpenIddictConstants.GrantTypes.Implicit
                 },
-                scopes: commonScopes,
+                scopes: clientScopes,
                 redirectUri: $"{webClientRootUrl}signin-oidc",
                 clientUri: webClientRootUrl,
                 postLogoutRedirectUri: $"{webClientRootUrl}signout-callback-oidc"
+            );
+        }
+
+        //Swagger Client
+        var swaggerClient = configurationSection["PetProjectEcommerce_Admin_Swagger:ClientId"];
+        if (!swaggerClient.IsNullOrWhiteSpace())
+        {
+            var swaggerRootUrl = configurationSection["PetProjectEcommerce_Admin_Swagger:RootUrl"].TrimEnd('/');
+
+            /* PetProjectEcommerce_Web client is only needed if you created a tiered
+             * solution. Otherwise, you can delete this client. */
+            await CreateApplicationAsync(
+                name: swaggerClient,
+                type: OpenIddictConstants.ClientTypes.Public,
+                consentType: OpenIddictConstants.ConsentTypes.Implicit,
+                displayName: "Swagger Admin Application",
+                secret: null,
+                grantTypes: new List<string> //Hybrid flow
+                {
+                    OpenIddictConstants.GrantTypes.AuthorizationCode,
+                },
+                scopes: adminScopes,
+                redirectUri: $"{swaggerRootUrl}/swagger/oauth2-redirect.html",
+                clientUri: swaggerRootUrl
             );
         }
 
