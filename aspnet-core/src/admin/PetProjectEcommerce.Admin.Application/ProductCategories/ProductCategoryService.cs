9c2e996d663d3d9789a2e3de2ec38bb2755ce58f
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-01-02 14:54:14 +0700

    Setup api crud for product category

---
+++ aspnet-core/src/admin/PetProjectEcommerce.Admin.Application/ProductCategories/ProductCategoryService.cs
@@ -0,0 +1,23 @@
+﻿using PetProjectEcommerce.ProductCategories;
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace PetProjectEcommerce.Admin.ProductCategories;
+
+public class ProductCategoryService :
+    CrudAppService<
+        ProductCategory, 
+        ProductCategoryDto, 
+        Guid, 
+        PagedResultRequestDto, 
+        CreateUpdateProductCategoryDto
+    >,
+    IProductCategoryService
+{
+    public ProductCategoryService(IRepository<ProductCategory, Guid> repository) 
+        : base(repository)
+    {
+    }
+}
