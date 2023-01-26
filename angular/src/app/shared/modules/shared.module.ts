d7b641bd2747d8981ebfee118929721ccf108e33
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-01-26 22:05:29 +0700

    validation form

---
+++ angular/src/app/shared/modules/shared.module.ts
@@ -0,0 +1,19 @@
+import { CoreModule } from "@abp/ng.core";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ValidationMessageComponent } from "./validation-message/validation-message.component";
+
+
+@NgModule({
+    imports: [
+        CoreModule,
+        CommonModule
+    ],
+    declarations: [
+        ValidationMessageComponent
+    ],
+    exports: [
+        ValidationMessageComponent
+    ]
+})
+export class Shared2Module {}
