b59f68f6d908fe85f7dc80ba8bb491b32124d18a
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-01-31 08:00:23 +0700

    Xóa sản phẩm

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from './shared/services/auth.service';
   selector: 'app-root',
   template: `
     <router-outlet></router-outlet>
+    <p-toast position="top-right"></p-toast>
+    <p-confirmDialog header="Xác nhận" acceptLabel="Có" rejectLabel="Không" icon="pi pi-exclamation-triangle"></p-confirmDialog>
   `,
 })
 export class AppComponent implements OnInit {
